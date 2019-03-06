61b6a5ad765471b4474a62131528b76cb78c9592
turuslan <turuslan.devbox@gmail.com>
2019-03-06 11:43:49 +0300

    create List.Ast

---
+++ src/Lisp/Ast.hs
@@ -0,0 +1,25 @@
+module Lisp.Ast where
+
+
+
+data SExpr
+  = Atom String
+  | IntegerLiteral Integer
+  | FloatLiteral Double
+  | StringLiteral String
+  | EmptyList
+  | DottedPair SExpr SExpr
+
+instance Show SExpr where
+  show (Atom name) = name
+  show (IntegerLiteral value) = show value
+  show (FloatLiteral value) = show value
+  show (StringLiteral value) = show value
+  show EmptyList = "()"
+  show xs'@(DottedPair _ _) = "(" ++ f xs' ++ ")"
+    where
+      f (DottedPair x xs) = show x ++ g xs
+      f _ = error "impossible"
+      g EmptyList = ""
+      g xs@(DottedPair _ _) = " " ++ f xs
+      g x = " . " ++ show x
