19acc08eeeb8b4b5ae628d1e8630e892d72cdedb
turuslan <turuslan.devbox@gmail.com>
2019-03-09 10:17:43 +0300

    + function

--- src/Lisp/Std.hs
+++ src/Lisp/Std.hs
@@ -13,6 +13,7 @@ initState = State [] []
   ]
   [ ("read-int", (parse_args "()", fun_read_int))
   , ("print", (parse_args "(x)", fun_print))
+  , ("+", (parse_args "(&rest xs)", fun__plus))
   ]
 
 parse_args :: String -> SExpr
@@ -36,3 +37,33 @@ fun_print [(_, arg)] = do
   eval_write $ show arg
   return arg
 fun_print _ = impossible
+
+fun__plus :: Fun
+fun__plus [(_, args)] = sum' args where
+  sum' EmptyList = return $ IntegerLiteral 0
+  sum' (DottedPair car cdr) = do
+    scdr <- sum' cdr
+    coerced <- coerce car scdr
+    return $ math_plus coerced
+  sum' _ = impossible
+fun__plus _ = impossible
+
+
+
+--
+coerce :: SExpr -> SExpr -> Eval (SExpr, SExpr)
+coerce a@(IntegerLiteral _) b@(IntegerLiteral _) = return (a, b)
+coerce a@(FloatLiteral _) b@(FloatLiteral _) = return (a, b)
+coerce (IntegerLiteral a) b@(FloatLiteral _) = return (IntegerLiteral $ fromInteger a, b)
+coerce a@(FloatLiteral _) (IntegerLiteral b) = return (a, IntegerLiteral $ fromInteger b)
+coerce a b = eval_error (show (if is_number a then b else a) ++ " is not a number")
+
+is_number :: SExpr -> Bool
+is_number (FloatLiteral _) = True
+is_number (IntegerLiteral _) = True
+is_number _ = False
+
+math_plus :: (SExpr, SExpr) -> SExpr
+math_plus (IntegerLiteral a, IntegerLiteral b) = IntegerLiteral (a + b)
+math_plus (FloatLiteral a, FloatLiteral b) = FloatLiteral (a + b)
+math_plus _ = impossible
