1c5ddd34a72a291f369a90e5653333055dbf78b9
turuslan <turuslan.devbox@gmail.com>
2019-03-10 20:47:04 +0300

    repl ":r", ":f path"

--- src/Lisp/Repl.hs
+++ src/Lisp/Repl.hs
@@ -2,7 +2,6 @@ module Lisp.Repl where
 
 import Lisp.Eval
 import Lisp.Parser
-import Lisp.Ast
 import Lisp.Std
 
 runRepl :: IO ()
@@ -13,13 +12,17 @@ repl state = do
   putStr "> "
   input <- getLine
   case input of
-    "(quit)" -> putStrLn "Bye!"
-    _ ->
-      case parseString input of
-        exprs -> do
-          (state', result) <- evalIO state $ foldl1 (>>) $ map (eval []) exprs
-          case result of 
-            Left v -> putStrLn $ show v
-            Right (LispError err) -> putStrLn ("error: " ++ err)
-          repl state'
-
+    ":q" -> return ()
+    ":r" -> repl initState
+    ':':'f':' ':path -> do
+      s <- readFile path
+      evalStr s
+    _ -> evalStr input
+  where
+    evalStr s = case parseString s of
+      exprs -> do
+        (state', result) <- evalIO state $ foldl1 (>>) $ map (eval []) exprs
+        case result of 
+          Left v -> putStrLn $ show v
+          Right (LispError err) -> putStrLn ("error: " ++ err)
+        repl state'
