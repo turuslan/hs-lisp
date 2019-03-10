e6f6958e6829e6ceb701b8a01ee470b8980a6379
Alik Khilazhev <alikhil@mail.ru>
2019-03-10 14:22:37 +0300

    Update repl.hs

--- src/Lisp/Repl.hs
+++ src/Lisp/Repl.hs
@@ -2,20 +2,24 @@ module Lisp.Repl where
 
 import Lisp.Eval
 import Lisp.Parser
-
-exitCommand = "(quit)"
+import Lisp.Ast
+import Lisp.Std
 
 runRepl :: IO ()
-runRepl = repl
+runRepl = repl initState
 
-repl :: IO ()
-repl = do
+repl :: State -> IO ()
+repl state = do
   putStr "> "
   input <- getLine
   case input of
     "(quit)" -> putStrLn "Bye!"
     _ ->
       case parseString input of
-        expr -> do
-          putStr $ show expr
-          repl
+        exprs -> do
+          (state', result) <- evalIO state $ foldl (>>) (return EmptyList) $ map (eval []) exprs
+          case result of 
+            Left v -> putStrLn $ show v
+            Right (LispError err) -> putStrLn ("error: " ++ err)
+          repl state'
+
