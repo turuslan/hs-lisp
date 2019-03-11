8bf1a5ab131d28fd78d9e17104aef6f0023dd993
turuslan <turuslan.devbox@gmail.com>
2019-03-11 17:11:10 +0300

    add bin-search example

---
+++ lisp-programs/bin-search.lisp
@@ -0,0 +1,23 @@
+
+(defun guess (ileft eright)
+  (if (= ileft (- eright 1))
+    (seq
+      (print "number is ")
+      (princ ileft)
+      ileft)
+    (let ((mid (floor (/ (+ ileft eright) 2))))
+      (seq
+        (print "less than ")
+        (princ mid)
+        (princ "?")
+        (if (= (read-int) 0)
+          (guess mid eright)
+          (guess ileft mid)
+          )))))
+
+(defun game ()
+  (guess
+    (seq (print "min (i): ") (read-int))
+    (+ (seq (print "max (i): ") (read-int)))))
+
+(game)
