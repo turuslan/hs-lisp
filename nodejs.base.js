ca3bbe6a05215eb7b5e02d6a8a99f3a8068e4629
turuslan <turuslan.devbox@gmail.com>
2019-04-07 18:12:19 +0300

    nodejs build target

---
+++ nodejs.base.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+const Fs = require('fs');
+
+// https://gist.github.com/espadrine/172658142820a356e1e0
+const SyncStdin = {
+  getStdin(endByte) {
+    var BUFSIZE = 256;
+    var buf = Buffer.alloc(BUFSIZE);
+    var totalBuf = Buffer.alloc(BUFSIZE);
+    var totalBytesRead = 0;
+    var bytesRead = 0;
+    var endByteRead = false;
+
+    var fd = process.stdin.fd;
+    // Linux and Mac cannot use process.stdin.fd (which isn't set up as sync).
+    var usingDevice = false;
+    try {
+      fd = Fs.openSync('/dev/stdin', 'rs');
+      usingDevice = true;
+    } catch (e) {}
+
+    for (;;) {
+      try {
+        bytesRead = Fs.readSync(fd, buf, 0, BUFSIZE, null);
+
+        // Copy the new bytes to totalBuf.
+        var tmpBuf = Buffer.alloc(totalBytesRead + bytesRead);
+        totalBuf.copy(tmpBuf, 0, 0, totalBytesRead);
+        buf.copy(tmpBuf, totalBytesRead, 0, bytesRead);
+        totalBuf = tmpBuf;
+        totalBytesRead += bytesRead;
+
+        // Has the endByte been read?
+        for (var i = 0; i < bytesRead; i++) {
+          if (buf[i] === endByte) {
+            endByteRead = true;
+            break;
+          }
+        }
+        if (endByteRead) { break; }
+      } catch (e) {
+        if (e.code === 'EOF') { break; }
+        throw e;
+      }
+      if (bytesRead === 0) { break; }
+    }
+    if (usingDevice) { Fs.closeSync(fd); }
+    return totalBuf;
+  },
+  buffered: '',
+  getline() {
+    if (this.buffered.length === 0) {
+      this.buffered = this.getStdin('\n'.charCodeAt(0)).toString('utf-8');
+    }
+    const newline = this.buffered.search('\n') + 1;
+    const line = this.buffered.slice(0, newline);
+    this.buffered = this.buffered.slice(newline);
+    return line;
+  },
+};
+
+program({
+  _global: {
+    ['read-int']() {
+      const value = +SyncStdin.getline();
+      if (isNaN(value)) {
+        throw new Error('not integer');
+      } else {
+        return value;
+      }
+    },
+    print(x) {
+      process.stdout.write(x + '\n');
+      return x;
+    },
+    princ(x) {
+      process.stdout.write(x + '');
+      return x;
+    },
+    floor(x) {
+      return Math.floor(x);
+    },
+  },
+  _def(name, fn) {
+    this._global[name] = fn;
+  },
+  _get(name) {
+    if (name in this._global) {
+      return this._global[name];
+    } else {
+      throw new Error(`name "${name}" not found`);
+    }
+  },
+});
